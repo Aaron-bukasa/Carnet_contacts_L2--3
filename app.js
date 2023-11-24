7d5c51fb50f37f4f93ee911ce1de816608f395dd
Aaron-bukasa <01bukasa@gmail.com>
2023-11-24 14:57:28 +0200

    ajout des fonctions creer le contact et reinitialiser le formulaire

---
+++ app.js
@@ -0,0 +1,106 @@
+// liste des contacts
+let contactList = [];
+
+// General variables
+
+// Variables des formulaires
+let prenom, nom, tel, groupe, email, bio, file, formContact, btnCreer, btnRenit;
+
+prenom = document.querySelector('#prenom');
+nom = document.querySelector('#nom');
+tel = document.querySelector('#tel');
+groupe = document.querySelector('#groupe');
+email = document.querySelector('#email');
+bio = document.querySelector('#bio');
+file = document.querySelector('#file');
+formContact = document.querySelector('.formContact');
+btnCreer = document.querySelector('.btn__submit');
+btnRenit = document.querySelector('.btn__reset');
+
+// Variables sur la liste des contacts
+let divContactList = document.querySelector('.listContact__content');
+const figureTemplate = document.querySelector('#figure__template');
+// let body = document.querySelector('body');
+
+
+// Events
+// formContact.addEventListener('keypress', (e) => e.preventDefault());
+btnCreer.addEventListener('click', addContact);
+// window.addEventListener('load', () => {
+//     contactList = JSON.parse(localStorage.getItem("tab"));
+//     viewContacts()
+// })
+btnRenit.addEventListener('click', formReset);
+
+
+/* LES VALIDATIONS */
+
+
+
+
+/* FONCTIONS */
+
+// La fonction qui reinitialise les champs du formulaire
+function formReset() {
+    prenom.value = "";
+    nom.value = "";
+    tel.value = "";
+    groupe.value = "";
+    email.value = "";
+    bio.value = "";
+    file.value = "";
+}
+
+// La fonction qui ajoute le contact dans le tableau des contacts
+function addContact(e) {
+    e.preventDefault();
+    const newContact = {
+        "prenom": prenom.value,
+        "nom": nom.value,
+        "tel": tel.value,
+        "groupe": groupe.value,
+        "email": email.value,
+        "bio": bio.value,
+        "file": file.value
+    }
+    contactList.push(newContact);
+    // localStorage.setItem("tab", JSON.stringify(contactList));
+    formReset();
+    viewContacts();
+}
+
+// la fonction qui supprime le contact
+function deleteContact() {
+
+}
+
+// la fonction qui modifie le contact
+function completedContact() {
+
+}
+
+// La fonction qui imprime chaque contact du tableau sur le DOM
+function viewContacts() {
+    for(let contact of contactList) {
+        let figureContent = figureTemplate.content.cloneNode(true);
+        divContactList.append(figureContent);
+
+        document.querySelector('.f-names').textContent = `${contact.prenom} ${contact.nom} - ${contact.groupe}`;
+        document.querySelector('.f-tel').textContent = `${contact.tel}`;
+        document.querySelector('.f-bio').textContent = `${contact.bio}`;
+
+        let figure = document.querySelector('.listContact__content figure');
+        figure.addEventListener('click', (e) => {
+            const containerElement = e;
+            const clickElement = e.target.tagName;
+            console.log(clickElement);
+            if(clickElement == 'svg' && clickElement.parentElement.classList == 'profilBtn__completed') {
+                console.log('coucou');
+            }
+            if(clickElement == 'svg' && clickElement.parentElement.classList == 'profilBtn__delete') {
+                console.log('delete');
+            }
+            
+        })
+    }
+}
